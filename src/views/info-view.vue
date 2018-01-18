85ddf33d8160b66b42b4663d74ef9c5e41405195
pwrstudio <pwrpntr@gmail.com>
2018-01-18 11:50:59 +0100

    work on #14

--- src/views/info-view.vue
+++ src/views/info-view.vue
@@ -1,20 +1,18 @@
 <template>
   <div class='info'>
 
-    <div class='info__main' v-html='main.container.about.info'/>
+    <div class='info__main'>
+      <div class='info__main__overlay'/>
+      <span v-html='main.container.about.info'/>
+    </div>
     <div class='info__credits'>
-      <svg class='info__credits__logo' id="Layer_1"
-           data-name="Layer 1"
-           xmlns="http://www.w3.org/2000/svg"
-           width="808.5204"
-           height="378.3499"
-           viewBox="0 0 808.5204 378.3499">
-        <polygon class="cls-1"
-                 points="755.212 85.751 656.642 296.394 654.888 38.804 571.964 0 433.284 296.394 431.53 38.804 348.606 0 209.926 296.394 208.173 38.804 125.248 0 0 267.653 54.049 292.946 157.989 70.827 157.705 341.451 236.556 378.35 381.347 70.827 381.063 341.451 459.914 378.35 604.705 70.827 604.42 341.451 683.272 378.35 808.52 110.696 755.212 85.751" />
-      </svg>
+      <div class='info__credits__overlay'/>
       <span v-html='main.container.about.credits'/>
     </div>
-    <div class='info__tech' v-html='main.container.about.tech'/>
+    <div class='info__tech'>
+      <div class='info__tech__overlay'/>
+      <span v-html='main.container.about.tech'/>
+    </div>
   </div>
 </template>
 
@@ -33,61 +31,59 @@ export default {
 @import '../style/helpers/_responsive.scss';
 @import '../style/_variables.scss';
 
-.info {
-  position: fixed;
-  top: 80px;
-  left: 0;
-  width: 100vw;
-  height: calc(100vh - 160px);
-  overflow: auto;
-  @include hide-scroll;
+@mixin bluroverlay {
+  &__overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: calc(100% + 60px);
+    height: 100%;
+    box-shadow: inset -20px -30px 30px $black;
+  }
+}
 
+@mixin infobox {
+  position: absolute;
+  padding: 30px;
+  color: white;
+  overflow-y: auto;
+  overflow-x: hidden;
+  @include hide-scroll;
+  @include bluroverlay;
+}
+
+@mixin secondary {
+  @include infobox;
+  width: calc(50vw - 100px);
+  font-size: 14px;
+  line-height: 20px;
+  top: calc(50vh + 80px);
+  height: calc(50vh - 200px);
+}
+
+@mixin primary {
+  @include infobox;
+  top: 80px;
+  left: 30px;
+  height: calc(50vh - 80px);
+  width: calc(100vw - 120px);
+  font-size: 20px;
+  line-height: 24px;
+}
+
+.info {
   &__main {
-    float: left;
-    color: white;
-    font-size: 20px;
-    line-height: 24px;
-    max-width: 70;
-    width: 60vw;
-    max-width: 90%;
-    padding: 30px;
-    // @include screen-size('small') {
-    //   width: 90vw;
-    //   font-size: 18px;
-    //   line-height: 20px;
-    // }
+    @include primary;
   }
+
   &__credits {
-    float: right;
-    padding: 30px;
-    color: white;
-    font-size: 14px;
-    line-height: 20px;
-    max-width: 70;
-    width: 30vw;
-    max-width: 90%;
-    padding-bottom: 80px;
-    @include screen-size('small') {
-      width: 90vw;
-      font-size: 18px;
-      line-height: 20px;
-    }
+    @include secondary;
+    left: 30px;
   }
+
   &__tech {
-    float: left;
-    color: white;
-    padding: 30px;
-    font-size: 14px;
-    line-height: 20px;
-    max-width: 70;
-    width: 30vw;
-    max-width: 90%;
-    padding-bottom: 160px;
-    @include screen-size('small') {
-      width: 90vw;
-      font-size: 18px;
-      line-height: 20px;
-    }
+    @include secondary;
+    left: calc(50vw + 10px);
   }
 }
 
